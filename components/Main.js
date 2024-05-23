b1d8fa4b46ac82a77adc82a4763bb8f7be139081
Ivan Yke Cadag <83697818+ayk-anonymous@users.noreply.github.com>
2024-05-23 12:02:18 +0800

    Update Main.js

--- components/Main.js
+++ components/Main.js
@@ -6,7 +6,7 @@ import LeftBox from "./components/LeftBox";
 import RightBox from "./components/RightBox";
 
 const Main = () => {
-  const regex1 = "(aa+bb) (a+b)* (a+b+ab+ba) + (a+b+ab+ba)* (aa+bab)* (a+b+aa) (a+b+bb+aa)*";
+  const regex1 = "(aa+bb) (a+b)* (a+b+ab+ba) (a+b+ab+ba)* (aa+bab)* (a+b+aa) (a+b+bb+aa)*";
   const regex2 =
     "((101)+(111)* + (100) + (1+0+11)* ) (1+0+01)*(111+000+101)(1+0)*";
   const [string, setString] = useState("");
